fc3b9cacd88d377af36594dfede76a3af138cf9e
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-26 16:36:30 +0000

    feat: add user controller to fetch available team members

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require("../models/User");
+
+exports.getAvailableTeamMembers = async (req, res) => {
+  try {
+    const users = await User.find(
+      { _id: { $ne: req.user._id } },
+      "name email role institution specialty"
+    );
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
