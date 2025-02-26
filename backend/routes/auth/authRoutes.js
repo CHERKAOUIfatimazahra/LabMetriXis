3a1565354928e9113df7df33bd4be0eae458f968
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-26 15:09:07 +0000

    feat: add password reset routes for improved user account management

--- backend/routes/auth/authRoutes.js
+++ backend/routes/auth/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../../controllers/authController"); 
+const authController = require("../../controllers/authController");
 
 // Route pour l'inscription d'un utilisateur
 router.post("/register", authController.register);
@@ -14,5 +14,10 @@ router.post("/login", authController.login);
 // Route pour la verification d'un OTP
 router.post("/verify-otp", authController.verifyOTP);
 
+// Route pour la réinitialisation du mot de passe
+router.post("/forgot-password", authController.forgetPassword);
+
+// Route pour la réinitialisation du mot de passe
+router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;
