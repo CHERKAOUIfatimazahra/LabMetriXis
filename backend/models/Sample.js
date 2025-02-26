d8f993f07e962d1cdaefe3a0332f9b51e23456d8
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-26 17:19:18 +0000

    feat: update Sample model to enhance attributes and enforce required fields

--- backend/models/Sample.js
+++ backend/models/Sample.js
@@ -2,32 +2,35 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const sampleSchema = new Schema({
-  identification: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
   type: { type: String, required: true },
-  quantity: { type: String, required: true },
-  storageConditions: [{ type: String }],
-  storageLocation: [{ type: String }],
-  technician: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  collectionDate: { type: Date },
+  quantity: { type: Number, required: true },
+  unit: { type: String, required: true },
+  storageConditions: { type: String },
+  collectionDate: { type: Date, required: true },
   expirationDate: { type: Date },
+  status: {
+    type: String,
+    enum: ["Pending", "In Analysis", "Analyzed", "Archived"],
+    default: "Pending",
+  },
   protocolFile: {
     fileName: { type: String },
     fileLocation: { type: String },
     fileType: { type: String },
     uploadDate: { type: Date, default: Date.now },
   },
-  sampleReport: { type: String, required: true },
-  status: {
-    type: String,
-    enum: ["Available", "In Use", "Depleted", "Compromised", "Reserved"],
-    default: "Available",
+  technicianResponsible: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
+  analysisReport: { type: String },
   project: {
     type: Schema.Types.ObjectId,
     ref: "Project",
+    required: true,
   },
   createdBy: {
     type: Schema.Types.ObjectId,
