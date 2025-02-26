e137a469acc1e277b2a57f18989ed201d30002c7
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-26 15:05:37 +0000

    fix: update role check in isOrganizer middleware to use 'chercheur' and improve access control

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -2,9 +2,10 @@
 const User = require("../models/User");
 
 exports.isOrganizer = async (req, res, next) => {
-    const user = await User.findById(req.user.id);
-    if (user.role !== "organizer") {
-        return res.status(403).json({ message: "Access denied" });
+
+  const user = await User.findById(req.user.id);
+    if (user.role !== "chercheur") {
+        return res.status(403).json({ message: "Forbidden" });
     }
-    next();
+  next();
 };
