653abfb4770306ea9d72a5bacaaa32541e0872a3
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-26 15:08:43 +0000

    feat: enhance User model with additional fields for improved user management

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   role: {
     type: String,
-    enum: ["Admin", "Chercheur", "Technicien"],
-    default: "Chercheur",
+    enum: ["admin", "chercheur", "technicien"],
+    default: "chercheur",
   },
+  institution: { type: String },
+  specialty: { type: String },
+  profileImage: { type: String },
+
   isVerified: { type: Boolean, default: false },
   otp: { type: String },
   otpExpires: { type: Date },
+  otpLastValidated: { type: Date },
   verificationToken: { type: String },
   lastOTPSentAt: { type: Date },
 });
